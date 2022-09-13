3ced1b815fb2f7008fd5a57b06f387d30460eff6
Tanner Mickelson <supertanner@gmail.com>
2022-09-13 12:49:12 +0000

    Add unit tests with Jest

---
+++ src/EnvConfig.ts
@@ -0,0 +1,14 @@
+/**
+ * Configuration loaded from environment variables.
+ */
+export type EnvConfig = {
+
+}
+
+/**
+ * Load config from environment variables.
+ * @return {EnvConfig} parsed from environment variables.
+ */
+export function loadEnvConfig(): EnvConfig {
+  
+}
